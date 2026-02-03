1a5615898b3d0daacdae8e3a4594b72f8a12d257
Muhammad Hunzala <164901659+m-hunzala@users.noreply.github.com>
2026-02-03 19:38:28 +0500

    Update vite.config.ts
    Set base path for Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/HBD/",
+});
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
